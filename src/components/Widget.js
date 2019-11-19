f96ed2b9c6a99b29961911ae02c00bb20c6cfa2d
Giusseppe Del Vecchio <giudelvecchio@gmail.com>
2019-11-19 18:36:00 -0500

    fixed timing of animation for desktop

--- src/components/Widget.js
+++ src/components/Widget.js
@@ -36,7 +36,7 @@ class Widget extends React.Component {
         try {
             const url = 'http://NflArrest.com/api/v1/team'
             const facts = await Axios.get(url)
-            console.log(facts.data)
+            // console.log(facts.data)
             this.setState(state => ({
                 teamCrimesData: [...state.facts, facts.data[0].Team_name, facts.data[0].arrest_count, facts.data[1].Team_name,]
             }))
@@ -54,7 +54,7 @@ class Widget extends React.Component {
 
         const teamFacts = this.state.teamCrimesData
         const fact2 = `${teamFacts[0]} and ${teamFacts[2]} are tied for lead in the NFL with ${teamFacts[1]} arrests`
-        console.log(teamFacts)
+        // console.log(teamFacts)
         
 
 
