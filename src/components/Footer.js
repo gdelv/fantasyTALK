f7d6dad92e06086ed04d7d6d819ba7d948689a17
Giusseppe Del Vecchio <giudelvecchio@gmail.com>
2019-11-22 09:06:53 -0500

    updated footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,13 +2,33 @@ import React from 'react'
 
 const footerStyle = {
     background:'#0C1428',
-    padding:'20px',
+    padding:'40px',
     color:'white'
 }
 
+
+const footerLinks = {
+    display: 'flex',
+    justifyContent:'space-around',
+    color: 'white',
+    paddingBottom:'20px',
+}
+
+const aStyle = {
+    textDecoration: 'underline',
+    color:'white',
+}
 const Footer = () => {
     return(
         <footer style={footerStyle}>
+            <div style={footerLinks}>
+                <a style={aStyle} href='htttp://www.google.com' target='_blank' rel="noopener noreferrer">Google</a>
+                <a style={aStyle} href='htttp://www.facebook.com' target='_blank' rel="noopener noreferrer">Facebook</a>
+                <a style={aStyle} href='htttp://www.instagram.com' target='_blank' rel="noopener noreferrer">Instagram</a>
+                <a style={aStyle} href='htttp://www.twitter.com' target='_blank' rel="noopener noreferrer">Twitter</a>
+
+            </div>
+            <h5>Copyright &copy; 2019 </h5>
         </footer>
     )
 }
